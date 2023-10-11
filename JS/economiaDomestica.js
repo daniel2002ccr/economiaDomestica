844a49558fb44a886a236f804e10288933bddaf5
alumnofp <alumnofp@FPB06.CALASANZSALAMANCA.local>
2023-10-11 10:55:19 +0200

    HTML terminado y avance en JS

--- JS/economiaDomestica.js
+++ JS/economiaDomestica.js
@@ -1,4 +1,32 @@
-function mostrarResumen(){
+let totalGasto = 0;
+let conceptos = {}
 
-    let pagos = [];
+function agregarPago(concepto, coste){
+    totalGasto += coste;
+    if (conceptos[concepto]) {
+        conceptos[concepto]++;
+    } else {
+        conceptos[concepto] = 1;
+    }
+}
+
+function mostrarResumen() {
+    const resumenElemento = document.getElementById('resumen');
+    const resumenContainer = document.createElement('div');
+
+    
+    resumenElemento.innerHTML = '';
+    resumenElemento.appendChild(resumenContainer);
+
+    for (const concepto in conceptos) {
+        const numPagos = conceptos[concepto];
+
+        const masDeUnPago = numPagos !== 1 ? 's' : '';
+
+        const resumenLinea = document.createElement('div');
+        resumenLinea.textContent = `${concepto} ---- ${numPagos} pago${masDeUnPago}`;
+        resumenContainer.appendChild(resumenLinea);
+    }
+
+    
 }
