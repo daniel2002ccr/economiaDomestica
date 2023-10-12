4742e30f8930a30ef888d0cfe2ba857facce248d
UsuarioLuna <UsuarioLuna@gmail.com>
2023-10-12 17:20:25 +0200

    economiaDomestica terminado

--- JS/economiaDomestica.js
+++ JS/economiaDomestica.js
@@ -1,32 +1,45 @@
-let totalGasto = 0;
-let conceptos = {}
+let totalGastos = 0;  
+const pagosPorConcepto = {};  
 
-function agregarPago(concepto, coste){
-    totalGasto += coste;
-    if (conceptos[concepto]) {
-        conceptos[concepto]++;
+function agregarPago(concepto, precio) {
+
+    totalGastos += precio;
+    
+    if (pagosPorConcepto[concepto]) {
+        pagosPorConcepto[concepto]++;
     } else {
-        conceptos[concepto] = 1;
+        pagosPorConcepto[concepto] = 1;
     }
 }
 
 function mostrarResumen() {
-    const resumenElemento = document.getElementById('resumen');
-    const resumenContainer = document.createElement('div');
 
+    const resumenDiv = document.getElementById('resumen');
+    resumenDiv.innerHTML = '<h2>Resumen de todos los gastos</h2>';
     
-    resumenElemento.innerHTML = '';
-    resumenElemento.appendChild(resumenContainer);
-
-    for (const concepto in conceptos) {
-        const numPagos = conceptos[concepto];
-
-        const masDeUnPago = numPagos !== 1 ? 's' : '';
-
-        const resumenLinea = document.createElement('div');
-        resumenLinea.textContent = `${concepto} ---- ${numPagos} pago${masDeUnPago}`;
-        resumenContainer.appendChild(resumenLinea);
-    }
-
+    //Ordeno los pagos de mayor a menor
+    const ordenarConceptos = Object.keys(pagosPorConcepto).sort((a, b) => pagosPorConcepto[b] - pagosPorConcepto[a]);
     
+    ordenarConceptos.forEach(concepto => {
+        resumenDiv.innerHTML += `<p>${concepto} ---- ${pagosPorConcepto[concepto]} pagos</p>`;
+    });
+    
+    //Introduzco el gasto final
+    resumenDiv.innerHTML += `<p>Gasto final: ${totalGastos} €</p>`;
+    
+    // Calcular media de los gastos
+    const totalConceptos = ordenarConceptos.length;
+    const gastoMedio = totalConceptos === 0 ? 0 : totalGastos / totalConceptos;
+    resumenDiv.innerHTML += `<p>Gasto medio: ${gastoMedio.toFixed(2)} €/concepto</p>`;
+    
+    // Meto de nuevo el boton de mostrar resumen, ya que antes al darle al mostrar desaparecía.
+    resumenDiv.innerHTML += '<button onclick="mostrarResumen()">Mostrar Resumen</button>';
+
+     //Despues de realizar todos los calculos, dejamos todo de nuevo vacio y a 0
+     totalGastos = 0;
+     for (const concepto in pagosPorConcepto) {
+         delete pagosPorConcepto[concepto];
+     }
+
 }
+
